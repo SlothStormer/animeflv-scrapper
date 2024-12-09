4a52949fda260f3124b9c4bbe1f85642aedbad80
Sloth <sepcalamar@gmail.com>
2024-12-09 04:59:29 -0300

    im bad at programming :(

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,2 @@
-export type {
-  AnimeInfo,
-  EpisodeInfo,
-  EpisodeServer,
-  LatestAnime,
-  NextEpisode,
-  OnAirAnime,
-  RelatedAnime,
-  SearchAnime,
-} from "./types";
-
-export * as Scrapper from "./functions";
+export type * from "./types";
+export * from "./functions";
